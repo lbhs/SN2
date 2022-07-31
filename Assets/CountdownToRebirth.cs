3654bf060a2e8438fec84ff9aebbb4db1b30dfb1
ssogo <43896422+sgsogo@users.noreply.github.com>
2022-07-31 04:48:11 -0700

    Added Energy Meter text, repeating ts animation

---
+++ Assets/CountdownToRebirth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownToRebirth : MonoBehaviour
+{
+    public GameObject RespawningManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RespawningManager = GameObject.Find("RespawnTranstitionStateEmptyGameObject");
+        RespawningManager.GetComponent<RepeatAnimationOfTransitionState>().StartRespawnCountdown();
+        StartCoroutine (Countdown());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(8);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/RepeatAnimationOfTransitionState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatAnimationOfTransitionState : MonoBehaviour
+{
+    public GameObject AnimatedTransitionState;
+    public float WaitTimeForRespawn;
+    public Vector3 InstantiationLocation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartRespawnCountdown()
+    {
+        StartCoroutine(RespawnTransitionStateModel());
+    }
+
+    public IEnumerator RespawnTransitionStateModel()
+    {
+        yield return new WaitForSeconds(WaitTimeForRespawn);
+        Instantiate(AnimatedTransitionState, InstantiationLocation, Quaternion.Euler(new Vector3(0, 10, 0)));
+        
+    }
+
+}
